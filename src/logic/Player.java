0c6468d5689b64a94977eb8c28db5e7fb2288ecf
Cole Mische <colemische@gmail.com>
2025-01-05 16:25:34 -0600

    added small bit of player logic with dice rolls i wish i could add more :(((((

--- src/logic/Player.java
+++ src/logic/Player.java
@@ -1,9 +1,28 @@
 package src.logic;
 
 public class Player {
+
     private final GameEngine engine;
 
     public Player(GameEngine engine) {
         this.engine = engine;
+    }
+
+
+
+
+
+    public int diceRoll(){
+        return (int)(Math.random() * 6) + 1;
+    }
+
+    public void startMove(){
+        move(diceRoll());
+    }
+
+    public void move(int rolls){
+
+        //Loop through the linked list, decrementing the roll by 1 each tile. At the end, check for a slide or ladder
+
     }
 }
